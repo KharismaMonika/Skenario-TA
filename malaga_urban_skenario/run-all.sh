39f08d177154161e6554d135a4eeddde0449f00a
riris <kharismamonika@gmail.com>
2018-03-27 16:22:14 +0700

    skenario30ms/04 aneh

--- malaga_urban_skenario/run-all.sh
+++ malaga_urban_skenario/run-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-for (( i = 5; i < 40; i+=5 )); do
+for (( i = 35; i < 40; i+=5 )); do
 	for (( j = 1; j <= 10; j++ )); do
 		echo "skenario"$i"ms/skenario"$j
 		cd "skenario"$i"ms/skenario"$j
@@ -10,14 +10,14 @@ for (( i = 5; i < 40; i+=5 )); do
 	done
 done
 
-for (( i = 10; i < 35; i+=5 )); do
-	for (( j = 1; j <= 10; j++ )); do
-		echo "skenario"$i"ms/skenario"$j
-		cd "skenario"$i"ms/skenario"$j
-		awk -f pdr.awk s-dsdv230.tr
-		awk -f throughput.awk s-dsdv230.tr
-		awk -f n2nd.awk s-dsdv230.tr
-		awk -f ro.awk s-dsdv230.tr
-		cd "../../" 
-	done
-done
+# for (( i = 10; i < 35; i+=5 )); do
+# 	for (( j = 1; j <= 10; j++ )); do
+# 		echo "skenario"$i"ms/skenario"$j
+# 		cd "skenario"$i"ms/skenario"$j
+# 		awk -f pdr.awk s-dsdv230.tr
+# 		awk -f throughput.awk s-dsdv230.tr
+# 		awk -f n2nd.awk s-dsdv230.tr
+# 		awk -f ro.awk s-dsdv230.tr
+# 		cd "../../" 
+# 	done
+# done
